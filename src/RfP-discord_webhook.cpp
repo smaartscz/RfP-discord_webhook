c8f4c47f2b3164cb057657932813d1b3946c752c
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-11-05 15:02:33 +0100

    Fixed issues with timezones
    Also added example file

--- src/RfP-discord_webhook.cpp
+++ src/RfP-discord_webhook.cpp
@@ -6,25 +6,22 @@ Rock For People Discord countdown
 
 
 */
-
-//------------------Knihovny------------------------------------//
-#include <WiFi.h>
-#include <Discord_WebHook.h>
 #include "settings.h"
-#include <ArduinoOTA.h>
-#include <WiFiClient.h>
-#include "rfp.h"
-#include <WebSerial.h>
+
 //------------------Základní konfigurace------------------------//
 Discord_Webhook discord;
-const char *ntpServer = "cz.pool.ntp.org";
-const long gmtOffset_sec = 0;
+const char *ntpServer = "pi.home";
 const int daylightOffset_sec = 3600;
+const int gmtOffset_sec = 3600;
 int hour_old;
 bool doOnce = false;
 unsigned long unixTime;
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServer, gmtOffset_sec, daylightOffset_sec);
+
 AsyncWebServer server(80);
+void manualRfP(String userId);
 
 // Získej UNIX čas
 unsigned long getTime()
@@ -38,15 +35,19 @@ unsigned long getTime()
   time(&now);
   return now;
 }
-
-void manualRfP(String userId);
-
-void handleRequest(AsyncWebServerRequest *request){
+void handleRfPRequest(AsyncWebServerRequest *request)
+{
   if (request->hasParam("id"))
   {
     String id = request->getParam("id")->value();
     manualRfP(id);
+    request->send(200, "text/plain", "Request received");
   }
+  // You can send a response back to the client if needed
+  request->send(200, "text/plain", "Request received");
+}
+void handleConfigRequest(AsyncWebServerRequest *request)
+{
   if (request->hasParam("reset"))
   {
     doOnce = false;
@@ -55,15 +56,21 @@ void handleRequest(AsyncWebServerRequest *request){
   // You can send a response back to the client if needed
   request->send(200, "text/plain", "Request received");
 }
-
+void handleRequest(AsyncWebServerRequest *request){
+  // You can send a response back to the client if needed
+  request->send(404, "text/plain", "Not found!");
+}
 void setupServer()
 {
   server.begin();
   server.on("/rfp", HTTP_GET, [](AsyncWebServerRequest *request)
-            { handleRequest(request); });
+            { handleRfPRequest(request); });
+  server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request)
+            { handleConfigRequest(request); }); 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { handleRequest(request); });                   
   WebSerial.println("Server started");
 }
-
 void recvMsg(uint8_t *data, size_t len)
 {
   WebSerial.println("Received Data...");
@@ -74,7 +81,6 @@ void recvMsg(uint8_t *data, size_t len)
   }
   WebSerial.println(d);
 }
-
 // Inicializace WiFi, Discord webhooku a NTP času
 void setup()
 {
@@ -83,7 +89,7 @@ void setup()
   discord.addWiFi(WiFi_ssid, WiFi_password);                                                                                                          // Nastavení WiFi
   discord.connectWiFi();                                                                                                                              // Připoj se k WiFi
   discord.send("If you're 555, I'm 666:love_you_gesture::fire:[.](https://tenor.com/view/666-digital-numbers-digital-numbers-flashing-gif-5944997)"); // Pošli zprávu o zapnutí
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);                                                                                           // Nastav NTP server
+  timeClient.begin();                                                                                                                                 // Nastav NTP server
   time_t now;
   while ((now = time(nullptr)) == 0)
   {
@@ -123,7 +129,6 @@ void setup()
   WebSerial.msgCallback(recvMsg);
   setupServer();
 }
-
 // Funkce na posílání zprávy
 void RfP()
 {
@@ -136,43 +141,43 @@ void RfP()
   switch (Zbyva)
   {
   case 300: // Kaja
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [300](https://media.discordapp.net/attachments/770062338564554798/954823564882739230/ezgif.com-gif-maker.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**300**](https://media.discordapp.net/attachments/770062338564554798/954823564882739230/ezgif.com-gif-maker.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 250: // Kaja2
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [250](https://cdn.discordapp.com/attachments/1064251020340899953/1125053321636483172/ezgif.com-video-to-gif_2.gif) dní!(<t:1718272800:R>):love_you_gesture::fire: ";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**250**](https://cdn.discordapp.com/attachments/1064251020340899953/1125053321636483172/ezgif.com-video-to-gif_2.gif) dní!(<t:1718272800:R>):love_you_gesture::fire: ";
     break;
   case 200: // BMTH
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [200](https://giphy.com/gifs/concert-bring-me-the-horizon-oliver-sykes-3sXu3d4qnRkI) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**200**](https://giphy.com/gifs/concert-bring-me-the-horizon-oliver-sykes-3sXu3d4qnRkI) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
-  case 150: // Architects
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [150](https://media.discordapp.net/attachments/1064251020340899953/1125052433144487936/ezgif.com-video-to-gif_1.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+  case 150: // Corey Taylor
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**150**](https://tenor.com/view/corey-taylor-skull-slasher-relax-gif-14853047) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 100: // PowerWolf
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [100](https://media.tenor.com/icraL6qB5pcAAAAC/powerwolf-fire-and-forgive.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**100**](https://media.tenor.com/icraL6qB5pcAAAAC/powerwolf-fire-and-forgive.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 50: // Electric Callboy
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [50](https://tenor.com/view/lets-do-it-again-fuck-yeah-electric-callboy-gif-26329254) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**50**](https://tenor.com/view/lets-do-it-again-fuck-yeah-electric-callboy-gif-26329254) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
-  case 31: // Linkin Park
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [31](https://media.tenor.com/6SEu35ns1pwAAAAC/linkin-park.gif) dní!(<t:1718272800:R>):love_you_gesture::fire:";
+  case 31: // Crystal Lake
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**31**](https://media.discordapp.net/attachments/1064251020340899953/1170720269556535336/Crystal_Lake_Curse.gif?ex=655a1137&is=65479c37&hm=aeadafdbb1a4f32ae08610a93d752ea557d167954e7e7826ef5cd0687a7a21de&=) dní!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 14: // Moshpit
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [dva](https://media.tenor.com/rObe8p1aJgUAAAAC/slam-head-banger.gif) týdny!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**dva**](https://tenor.com/view/avril-lavigne-%EC%97%90%EC%9D%B4%EB%B8%8C%EB%A6%B4-%EB%9D%BC%EB%B9%88-gif-7558323830048710247) týdny!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 7: // RFP 2023
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [týden](https://cdn.discordapp.com/attachments/1064251020340899953/1125051885703929876/ezgif.com-video-to-gif.gif)!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**týden**](https://cdn.discordapp.com/attachments/1064251020340899953/1125051885703929876/ezgif.com-video-to-gif.gif)!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 3: // Slipknot
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [3](https://media.tenor.com/aBAuw7ALCBEAAAAC/slipknot-duality.gif) dny!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**3**](https://media.tenor.com/aBAuw7ALCBEAAAAC/slipknot-duality.gif) dny!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 2: // Rammstein
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [2](https://media.tenor.com/LtXzSqzcK3MAAAAC/dovperestroika.gif) dny!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**2**](https://media.tenor.com/LtXzSqzcK3MAAAAC/dovperestroika.gif) dny!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 1: // Architects
-    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [zííííííííítra](https://media.tenor.com/gUUkhAnr5hcAAAAC/tenside-tensidemusic.gif)!(<t:1718272800:R>):love_you_gesture::fire:";
+    message = "Dobré FUCKIN poledne, <@&1121821428111646864> je za [**zííííííííítra**](https://media.tenor.com/gUUkhAnr5hcAAAAC/tenside-tensidemusic.gif)!(<t:1718272800:R>):love_you_gesture::fire:";
     break;
   case 0: // Slipknot headbang
-    message = "PRÁVĚ TO VŠECHNO ZAČÍNÁ, <@&1121821428111646864>! [ENJOY](https://media.tenor.com/BQo9mt76WsYAAAAC/slipknot-drums.gif):love_you_gesture::fire:";
+    message = "PRÁVĚ TO VŠECHNO ZAČÍNÁ, <@&1121821428111646864>! [**ENJOY**](https://media.tenor.com/BQo9mt76WsYAAAAC/slipknot-drums.gif):love_you_gesture::fire:";
     break;
   default:
     message = "Dobré poledne, <@&1121821428111646864> je za ";
@@ -184,7 +189,6 @@ void RfP()
   }
   discord.send(message); // Pošli zprávu na discord
 }
-
 void manualRfP(String userID)
 {
   unixTime = getTime();
@@ -213,7 +217,8 @@ void sendTime(int currTime)
 }
 void onStartup()
 {
-  if (doOnce == false){
+  if (doOnce == false)
+  {
     delay(1000);
     WebSerial.print(F("IP address: "));
     WebSerial.println(WiFi.localIP());
@@ -222,15 +227,15 @@ void onStartup()
     doOnce = true;
   }
 }
-void loop(){
+void loop()
+{
+  timeClient.update(); // Update the current time
   onStartup();
-  struct tm timeinfo;    
-  if (!getLocalTime(&timeinfo))
-  {
-    WebSerial.println("Nelze získat aktuální čas");
-    return;
-  }
-  sendTime(timeinfo.tm_hour);
-  RfP_check();
+  sendTime(timeClient.getHours());
+
+if (timeClient.getHours() == 12 && timeClient.getMinutes() == 00 && timeClient.getSeconds() == 0) {
+    RfP(); // Trigger RfP funkci
+    delay(60000); // Add a delay to avoid calling the function multiple times in one minute
+}
   ArduinoOTA.handle();
 }
